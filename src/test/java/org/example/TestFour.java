b239a1a4310cb74f89d478e8e44402257e158f4f
Satyam-1516 <str.satyam15@gmail.com>
2023-04-21 10:05:43 +0530

    4th commit

--- src/test/java/org/example/TestFour.java
+++ src/test/java/org/example/TestFour.java
@@ -3,6 +3,7 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,7 +23,10 @@ public class TestFour {
 
     driver.findElement(By.xpath("//select[@id='dropdowm-menu-1']")).sendKeys("SQL");
     Thread.sleep(8000);
-    driver.findElement(By.xpath("//select[@id='dropdowm-menu-2']")).sendKeys("JUnit");
+    Select select2=new Select(driver.findElement(By.xpath("//select[@id='dropdowm-menu-2']")));
+    select2.selectByValue("junit");
+//    Select select3=new Select(driver.findElement(By.xpath("//select[@id='dropdowm-menu-3']")));
+//    select3.selectByValue("JQuery");
     driver.findElement(By.xpath("//select[@id='dropdowm-menu-3']")).sendKeys("JQuery");
 
     driver.findElement(By.xpath("//input[@value='option-1']")).click();
